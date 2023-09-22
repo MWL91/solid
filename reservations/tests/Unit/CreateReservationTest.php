9179064b53004b7e6a5ba334b18437ea6a4fb278
Marcin Lenkowski <marcin@lenkowski.net>
2023-09-22 18:21:54 +0200

    stage/7 - Test written

--- reservations/tests/Unit/CreateReservationTest.php
+++ reservations/tests/Unit/CreateReservationTest.php
@@ -3,9 +3,25 @@ declare(strict_types=1);
 
 namespace Reservations\Tests\Unit;
 
+use Ramsey\Uuid\Uuid;
 use Reservations\DomainModel\Commands\CreateOfficeCommand;
 use Reservations\DomainModel\OfficeAvailability;
 
 final class CreateReservationTest extends \PHPUnit\Framework\TestCase
 {
+    public function testItShouldCreateOffice(): void
+    {
+        // Given:
+        $officeId = Uuid::uuid4();
+        $createOfficeCommand = new CreateOfficeCommand(
+            $officeId,
+            'Office 1'
+        );
+
+        // When:
+        $office = OfficeAvailability::create($createOfficeCommand);
+
+        // Then:
+        $this->assertInstanceOf(OfficeAvailability::class, $office);
+    }
 }

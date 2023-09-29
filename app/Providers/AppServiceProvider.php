48a0708966a48570d8709f83a2c32a426b58961d
Marcin Lenkowski <marcin@lenkowski.net>
2023-09-29 20:43:59 +0200

    solid/4

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,16 +2,24 @@
 
 namespace App\Providers;
 
+use App\Repositories\OrderRepository;
+use App\Services\Interfaces\IOrdersService;
+use App\Services\OrdersService;
 use Illuminate\Support\ServiceProvider;
+use App\Repositories\Eloquent\OrderRepository as EloquentOrderRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public $bindings = [
+        OrderRepository::class => EloquentOrderRepository::class,
+        IOrdersService::class => OrdersService::class
+    ];
+
     /**
      * Register any application services.
      */
     public function register(): void
     {
-        //
     }
 
     /**
